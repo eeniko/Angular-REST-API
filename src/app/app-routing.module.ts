5b9b1d2cc3448f8f4f5579440103ea31414a6f32
eeniko <eniko179@gmail.com>
2022-10-05 21:55:07 +0200

    1st version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewUserComponent } from './users/new-user/new-user.component';
+import { UsersComponent } from './users/users.component';
+import { UpdateUserComponent } from './users/update-user/update-user.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [ 
+  {path:'',component: UsersComponent},
+{path:'users/new',component: NewUserComponent},
+{path:'users/update/:id',component: UpdateUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+import { NewUserComponent } from './users/new-user/new-user.component';
+import { UpdateUserComponent } from './users/update-user/update-user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NewUserComponent,
+    UpdateUserComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule, 
+    HttpClientModule, 
+    FormsModule, 
+    ReactiveFormsModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
